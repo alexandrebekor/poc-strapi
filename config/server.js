bdd9f40efeedc83c0d4fc1403c1a206d605993d6
Alexandre Bekor <contato@alexandrebekor.com>
2021-11-25 02:55:54 -0300

    start project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2eb08c1ba10c1347aa8b44aafab562ff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '00beefa3-69fd-488a-bd82-22e472097470'
+};
